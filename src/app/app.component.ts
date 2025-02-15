7933bfd39bf84bd1eb312dd6baaf6a58c9bdfc55
ShervilG <wintergpt45@gmail.com>
2025-02-15 14:47:36 +0000

    Added registration page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api/api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'chatty-patty-client';
+  constructor(private readonly ApiService: ApiService) { }
+
+  title = 'Welcome to Chatty Patty';
+  name: string = "";
+  registrationResult = "";
+  
+
+  registerName() {
+    if (this.name === "") {
+      alert("Please enter a name");
+      return;
+    }
+
+    this.ApiService.registerClient(this.name).subscribe((response) => {
+      this.registrationResult = response.message;
+    });
+  }
 }
